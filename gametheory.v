a3333399063c5fb25cc6f04aba6052c4b6442f68
Sam Nolan <samnolan555@gmail.com>
2024-09-26 17:12:34 +1000

    Add simple max/min commutativity theroems

--- gametheory.v
+++ gametheory.v
@@ -1,6 +1,8 @@
 (* This is an attempt to write theory of games and economic behaviour into Coq to prove to myself that I understand the contents *)
 
 Require Import QArith.
+Require Import Coq.Structures.Orders.
+Require Import Coq.Structures.OrdersTac.
 
 Module ReducedGame.
   (* We first start by defining the concept of a reduced game. In a reduced game, all players simply specify a strategy by it's index (a natural number) and then the value function determines the expected outcome for that collection of strategies. We then go ahead and define what it means to be a 0 sum game *)
@@ -25,34 +27,89 @@ Module ReducedGame.
     | zeroSumGame (g: ReducedGame) (t: game_is_zero_sum g).
 End ReducedGame.
 
-Module FunctionalCalculus.
-  (* We then need to define our functional calculus terms, min and max *)
+Module Type MinMax( E:TotalOrder').
+  Import E. 
+  Parameter X : Type.
+  Parameter min : (X -> t) -> t.
+  Axiom min_exists :
+    forall f, exists x0, min f = f x0.
+  Axiom min_is_minimum : forall f x, min f <= f x.
+  Parameter max : (X -> t) -> t.
+  Axiom max_exists :
+    forall f, exists x0, max f = f x0.
+  Axiom max_is_maximum : forall f x, f x <= max f.
+End MinMax.
 
-  (* This poses our first challenge... minimum is technically a function which given a function returns another function... But it's a tad difficult when it's curried. I want to at least try though. *)
-  Definition minimum_assumed_at {X: Type} (f: X -> Q) (x0:X): Prop:=
-    exists m, m = f x0 /\ forall x:X, m <= f x0.
+Module FunctionalCalculus (MinMaxMod : MinMax) (E:TotalOrder').
+  Module EMinMaxMod := MinMaxMod(E). 
+  Import EMinMaxMod. 
+  Import E.
+  Module OrderTac := MakeOrderTac(E)(E).  
+  Import OrderTac.
 
-  Definition maximum_assumed_at {X: Type} (f: X -> Q) (x0:X): Prop:=
-    exists m, m = f x0 /\ forall x:X, m >= f x0.
-
-  Definition is_minimum_f {X Y: Type} (f:X -> Y -> Q) (f2 : X -> Q): Prop :=
-    forall x y0, minimum_assumed_at (f x) y0 -> f2 x = f x y0. 
-
-  Definition is_maximum_f {X Y: Type} (f:X -> Y -> Q) (f2 : X -> Q): Prop :=
-    forall x y0, maximum_assumed_at (f x) y0 -> f2 x = f x y0. 
-
-  Theorem max_max_doesnt_care_variable_order : forall (X Y: Type) (f : X -> Y -> Q) (f2: X -> Q) (f3: Y -> Q) (x0: X) (y0: Y), is_maximum_f f f2 -> maximum_assumed_at f2 x0 -> is_maximum_f (Basics.flip f) f3 -> maximum_assumed_at f3 y0 -> f2 x0 = f3 y0.
+  Lemma le_trans : forall a b c, a <= b -> b <= c -> a <= c.
   Proof.
-    intros X Y f f2 f3 x0 y0 Hmax1y Hmax1x Hmax2x Hmax2y.
-    unfold is_maximum_f in Hmax1y.
-    rewrite -> (Hmax1y x0 y0).
-    unfold is_maximum_f in Hmax2x.
-    rewrite -> (Hmax2x y0 x0).
-    unfold Basics.flip. 
-    reflexivity.
+    order. 
   Qed.
-  Theorem max_min_always_smaller_min_max : forall (X Y: Type) (f : X -> Y -> Q) (f2: X -> Q) (f3: Y -> Q) (x0: X) (y0: Y), is_minimum_f f f2 -> maximum_assumed_at f2 x0 -> is_maximum_f (Basics.flip f) f3 -> minimum_assumed_at f3 y0 -> f2 x0 <= f3 y0.
+
+
+  (* First we prove that if you find the minimum over two variables,
+     it must necesarily be the minimum of the bivariate function *)
+  Lemma min_min_is_min_of_both : forall (f: X -> X -> t) x y, min (fun x => min (f x)) <= (f x y).
   Proof.
+    intros f x y. 
+    apply (le_trans (min (fun x => min (f x))) (min (f x)) (f x y)).
+    - apply (min_is_minimum (fun x => min (f x)) x). 
+    - apply min_is_minimum. 
+  Qed.
+
+  Lemma lower_a_b_eq : forall a b, a <= b -> b <= a -> b == a.
+  Proof.
+    order. 
+  Qed.
+
+
+  (* Therefore this means that the both the left and the right of this
+     are lower than or equal to all f x y, hence lower than or equal
+     to each other, hence equal *)
+  Theorem min_min_commutative : forall (f: X -> X -> t), min (fun x => min (f x)) == min (fun x => min (Basics.flip f x)).
+  Proof.
+    intros f.
+    apply lower_a_b_eq. 
+    - destruct (min_exists (fun x => min (f x))) as [y0r ->].
+      destruct (min_exists (f y0r)) as [x0r ->].
+      fold ((Basics.flip f) x0r y0r). 
+      apply min_min_is_min_of_both. 
+    - destruct (min_exists (fun x : X => min (Basics.flip f x))) as [y0r ->].
+      destruct (min_exists (Basics.flip f y0r)) as [x0r ->].
+      unfold Basics.flip. 
+      apply min_min_is_min_of_both. 
+  Qed.
+
+  (* Same argument goes for maximum *)
+  Lemma max_max_is_max_of_both : forall (f: X -> X -> t) x y, (f x y) <= max (fun x => max (f x)).
+  Proof.
+    intros f x y. 
+    apply (le_trans (f x y) (max (f x)) (max (fun x0 : X => max (f x0))) ).
+    - apply max_is_maximum.
+    - apply (max_is_maximum (fun x => max (f x)) x). 
+  Qed.
+
+  Theorem max_max_commutative : forall (f: X -> X -> t), max (fun x => max (f x)) == max (fun y => max (Basics.flip f y)).
+  Proof.
+    intros f.
+    apply lower_a_b_eq. 
+    - destruct (max_exists (fun y : X => max (Basics.flip f y))) as [y0r ->].
+      destruct (max_exists (Basics.flip f y0r)) as [x0r ->].
+      unfold Basics.flip. 
+      apply max_max_is_max_of_both. 
+    - destruct (max_exists (fun x => max (f x))) as [y0r ->].
+      destruct (max_exists (f y0r)) as [x0r ->].
+      fold (Basics.flip f x0r y0r). 
+      apply max_max_is_max_of_both. 
+  Qed.
+
+
 
 
 End FunctionalCalculus.
